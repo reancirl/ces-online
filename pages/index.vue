b618f3f496004ebb6271f76fd89e9203c00d86f9
BAL <reancirl@gmail.com>
2020-04-27 15:13:35 +0800

    about init

--- pages/index.vue
+++ pages/index.vue
@@ -5,17 +5,34 @@
 			<h1 class="outline-1">What you crave?</h1>
 		</div>
   	</div>
-		<div id="hours">
-			<h1 class="outline-2 text-center mt-3">Business Hours</h1>
-			<div class="container pt-3">
-				<b-row>
-					<b-col sm="6"><img src="../assets/hamburger-1.svg" style="width:90%;"></b-col>
-					<b-col sm="6">
-						<h4></h4>
-					</b-col>
-				</b-row>
-			</div>
+	<div id="hours">
+		<h1 class="outline-2 text-center mt-3">Business Hours</h1>
+		<div class="container pt-3">
+			<b-row>
+				<b-col sm="6"><img src="../assets/hamburger-1.svg" style="width:90%;"></b-col>
+				<b-col sm="6" class="">
+					<h4><span class="day-1">Monday :</span> (8:00 am - 6:00pm)</h4>
+					<h4><span class="day-1">Tuesday :</span> (8:00 am - 6:00pm)</h4>
+					<h4><span class="day-1">Wednesday :</span> (8:00 am - 6:00pm)</h4>
+					<h4><span class="day-1">Thursday :</span> (8:00 am - 6:00pm)</h4>
+					<h4><span class="day-1">Friday :</span> (8:00 am - 6:00pm)</h4>
+					<h4><span class="day-1">Saturday :</span> (8:00 am - 8:00 pm)</h4>
+					<h4><span class="day-1">Sunday :</span> (1:00 pm - 8:00 pm)</h4>
+				</b-col>
+			</b-row>
 		</div>
+	</div>
+	<div id="about">
+		<h1 class="outline-2 text-center mt-3">About us</h1>
+		<div class="container pt-3">
+			<b-row>
+				<b-col sm="6" class="">
+					
+				</b-col>
+				<b-col sm="6"><img src="../assets/shopping-1.svg" style="width:90%;"></b-col>
+			</b-row>
+		</div>
+	</div>
   </div>
 </template>
 
@@ -59,7 +76,13 @@
 		font-size: 3rem;		
 		font-weight: 900;
 		color: #c6a530 !important;
-		-webkit-text-stroke:1.5px #fff;
+		-webkit-text-stroke:1.7px black;
+	}
+	.day-1{
+		font-size: 1.1rem;
+		color:#c6a530;
+		font-weight: 700;
+		-webkit-text-stroke:.5px black;
 	}
 	@media (max-width: 415px){
 		.outline-1 {
