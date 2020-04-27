601d47f74b88699b79ae36752d6357ea991c300f
BAL <reancirl@gmail.com>
2020-04-27 22:37:52 +0800

    edit config nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,9 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    titleTemplate(titleChunk) {
+      return titleChunk ? titleChunk : 'Yong Snack Yard & Frozen Products'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
